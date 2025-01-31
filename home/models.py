4f5424b75971efe2246fbdb133720fa41a59d5a8
Artem <av3411261@gmail.com>
2025-01-31 22:53:16 +0200

    Made the database structure, working on qr codes

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+
+class Subscribers(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE),
+    subscriber = models.CharField(max_length=255)
+
+class qr_code(models.Model):
+    image = models.ImageField(upload_to="images/"),
+    name = models.CharField(max_length=50),
+    size = models.IntegerField(),
+    shape = models.IntegerField(),
+    custom_style = models.CharField(max_length=255)
+    link = models.TextField(),
+    data_create = models.IntegerField(),
+    expiry_date = models.IntegerField()
+
--- home/views.py
+++ home/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db.utils import IntegrityError 
 from django.contrib.auth import authenticate, login, logout
+from django.core.files.base import ContentFile
+import qrcode
+from io import BytesIO
+from home.models import qr_code
 
 def render_home(request):
     return render(request=request, template_name='base.html')
@@ -55,6 +59,32 @@ def render_contacts(request):
 
 def render_generator(request):
     if request.user.is_authenticated:
+        if request.method == "POST":
+            qr_link = request.POST.get("link_or_text")
+
+            qr = qrcode.QRCode(
+                version=1,
+                box_size=10,
+                border=4
+            )
+            qr.add_data(qr_link)
+            qr.make(fit=True)
+        
+            img = qr.make_image(fill_color="black", back_color="white")
+
+            image_io = BytesIO()
+            img.save(image_io, format="PNG")
+
+            # Создаем объект модели
+            qr_code1 = qr_code()
+
+            # Сбрасываем указатель потока в начало
+            image_io.seek(0)
+
+            # Сохраняем изображение в модель без tuple
+            qr_code1.image.save(f"qr_code.png", ContentFile(image_io.getvalue()), save=True)
+        
+            return render(request, "generator.html")
         return render(request, "generator.html")
     else:
         return redirect("registration")
@@ -66,3 +96,10 @@ def render_history_gen(request):
 def logout_user(request):
     logout(request)
     return redirect('authorization')
+
+
+
+
+
+
+
